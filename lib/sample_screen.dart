17c443311217f98e6e55b7a54ff82d07f4bce54f
satochibi <satochibi0120@gmail.com>
2022-04-03 15:06:52 +0900

    CardをmacOSに対応

--- lib/sample_screen.dart
+++ lib/sample_screen.dart
@@ -32,154 +32,163 @@ class SampleScreen extends StatelessWidget {
           ),
         ),
         body: SizedBox(
-          width: double.infinity,
-          child: ListView(children: [
-            Card(
-              elevation: 4.0,
-              margin: const EdgeInsets.all(10.0),
-              child: Row(
-                crossAxisAlignment: CrossAxisAlignment.center,
-                children: [
-                  Container(
-                    padding: const EdgeInsets.all(8),
-                    child: ArtBoard(
-                      height: 105,
-                      aspectRatioW: 4,
-                      aspectRatioH: 3,
-                      isDrawable: true,
+            width: double.infinity,
+            child: ListView(children: [
+              Card(
+                elevation: 4.0,
+                margin: const EdgeInsets.all(10.0),
+                child: Row(
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    Container(
+                      padding: const EdgeInsets.all(8),
+                      child: ArtBoard(
+                        height: 105,
+                        aspectRatioW: 4,
+                        aspectRatioH: 3,
+                        isDrawable: true,
+                      ),
                     ),
-                  ),
-                  Flexible(
-                    child: Container(
+                    Flexible(
+                      child: Container(
                         margin: const EdgeInsets.only(
                             top: 10, right: 10, bottom: 10),
                         child: SizedBox(
                           height: 105,
                           child: Column(
                             mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                            children: <Widget>[
-                              const SizedBox(
-                                width: double.infinity,
-                                child: Text(
-                                  "イーブイを描いてみた",
-                                  style: TextStyle(
-                                      fontWeight: FontWeight.bold,
-                                      fontSize: 12),
-                                  overflow: TextOverflow.ellipsis,
-                                  textAlign: TextAlign.left,
-                                ),
-                              ),
-                              const SizedBox(
-                                width: double.infinity,
-                                child: Text(
-                                  "前に描いたものにいろいろ書き足してみました。動きはそのまんまですけどね。ところで、この作品を作るにあたって、どんなところ",
-                                  style: TextStyle(fontSize: 10),
-                                  overflow: TextOverflow.ellipsis,
-                                  maxLines: 3,
-                                  textAlign: TextAlign.left,
-                                ),
-                              ),
-                              Container(
-                                color: const Color(0xffeeeeee),
-                                width: double.infinity,
-                                height: 14,
-                                child: Row(
-                                  mainAxisAlignment:
-                                      MainAxisAlignment.spaceBetween,
-                                  children: [
-                                    ClipRRect(
-                                      borderRadius: BorderRadius.circular(3),
-                                      child: Container(
-                                        width: 22,
-                                        color: const Color(0xffcfcfcf),
-                                        child: const FittedBox(
-                                          child: Icon(Icons.play_arrow,
-                                              color: Colors.white),
-                                        ),
-                                      ),
+                            children: [
+                              Column(
+                                children: const [
+                                  SizedBox(
+                                    width: double.infinity,
+                                    child: Text(
+                                      "イーブイを描いてみた",
+                                      style: TextStyle(
+                                          fontWeight: FontWeight.bold,
+                                          fontSize: 12),
+                                      overflow: TextOverflow.ellipsis,
+                                      textAlign: TextAlign.left,
                                     ),
-                                    const FittedBox(
-                                      fit: BoxFit.fitHeight,
-                                      child: Text(
-                                        "110,489",
-                                        style: TextStyle(
-                                            fontWeight: FontWeight.bold,
-                                            color: Color(0xff777777)),
-                                      ),
-                                    )
-                                  ],
-                                ),
-                              ),
-                              Container(
-                                color: const Color(0xffffeeb9),
-                                width: double.infinity,
-                                height: 14,
-                                child: Row(
-                                  mainAxisAlignment:
-                                      MainAxisAlignment.spaceBetween,
-                                  children: [
-                                    ClipRRect(
-                                      borderRadius: BorderRadius.circular(3),
-                                      child: Container(
-                                        width: 22,
-                                        color: const Color(0xffffa600),
-                                        child: const FittedBox(
-                                          child: Icon(Icons.star,
-                                              color: Colors.white),
-                                        ),
-                                      ),
+                                  ),
+                                  SizedBox(
+                                    width: double.infinity,
+                                    child: Text(
+                                      "前に描いたものにいろいろ書き足してみました。動きはそのまんまですけどね。ところで、この作品を作るにあたって、どんなところ",
+                                      style: TextStyle(fontSize: 10),
+                                      overflow: TextOverflow.ellipsis,
+                                      maxLines: 3,
+                                      textAlign: TextAlign.left,
                                     ),
-                                    const FittedBox(
-                                      fit: BoxFit.fitHeight,
-                                      child: Text(
-                                        "1,121",
-                                        style: TextStyle(
-                                            fontWeight: FontWeight.bold,
-                                            color: Color(0xfff58206)),
-                                      ),
-                                    )
-                                  ],
-                                ),
+                                  ),
+                                ],
                               ),
-                              Container(
-                                color: const Color(0xffdcf1ff),
-                                width: double.infinity,
-                                height: 14,
-                                child: Row(
-                                  mainAxisAlignment:
-                                      MainAxisAlignment.spaceBetween,
-                                  children: [
-                                    ClipRRect(
-                                      borderRadius: BorderRadius.circular(3),
-                                      child: Container(
-                                        width: 22,
-                                        color: const Color(0xff2693FF),
-                                        child: const FittedBox(
-                                          child: Icon(Icons.download,
-                                              color: Colors.white),
+                              Column(
+                                children: [
+                                  Container(
+                                    color: const Color(0xffeeeeee),
+                                    width: double.infinity,
+                                    height: 14,
+                                    child: Row(
+                                      mainAxisAlignment:
+                                          MainAxisAlignment.spaceBetween,
+                                      children: [
+                                        ClipRRect(
+                                          borderRadius:
+                                              BorderRadius.circular(3),
+                                          child: Container(
+                                            width: 22,
+                                            color: const Color(0xffcfcfcf),
+                                            child: const FittedBox(
+                                              child: Icon(Icons.play_arrow,
+                                                  color: Colors.white),
+                                            ),
+                                          ),
                                         ),
-                                      ),
+                                        const FittedBox(
+                                          fit: BoxFit.fitHeight,
+                                          child: Text(
+                                            "110,489",
+                                            style: TextStyle(
+                                                fontWeight: FontWeight.bold,
+                                                color: Color(0xff777777)),
+                                          ),
+                                        )
+                                      ],
                                     ),
-                                    const FittedBox(
-                                      fit: BoxFit.fitHeight,
-                                      child: Text(
-                                        "3,334",
-                                        style: TextStyle(
-                                            fontWeight: FontWeight.bold,
-                                            color: Color(0xff2984e2)),
-                                      ),
-                                    )
-                                  ],
-                                ),
+                                  ),
+                                  Container(
+                                    color: const Color(0xffffeeb9),
+                                    width: double.infinity,
+                                    height: 14,
+                                    child: Row(
+                                      mainAxisAlignment:
+                                          MainAxisAlignment.spaceBetween,
+                                      children: [
+                                        ClipRRect(
+                                          borderRadius:
+                                              BorderRadius.circular(3),
+                                          child: Container(
+                                            width: 22,
+                                            color: const Color(0xffffa600),
+                                            child: const FittedBox(
+                                              child: Icon(Icons.star,
+                                                  color: Colors.white),
+                                            ),
+                                          ),
+                                        ),
+                                        const FittedBox(
+                                          fit: BoxFit.fitHeight,
+                                          child: Text(
+                                            "1,121",
+                                            style: TextStyle(
+                                                fontWeight: FontWeight.bold,
+                                                color: Color(0xfff58206)),
+                                          ),
+                                        )
+                                      ],
+                                    ),
+                                  ),
+                                  Container(
+                                      color: const Color(0xffdcf1ff),
+                                      width: double.infinity,
+                                      height: 14,
+                                      child: Row(
+                                          mainAxisAlignment:
+                                              MainAxisAlignment.spaceBetween,
+                                          children: [
+                                            ClipRRect(
+                                              borderRadius:
+                                                  BorderRadius.circular(3),
+                                              child: Container(
+                                                width: 22,
+                                                color: const Color(0xff2693FF),
+                                                child: const FittedBox(
+                                                  child: Icon(Icons.download,
+                                                      color: Colors.white),
+                                                ),
+                                              ),
+                                            ),
+                                            const FittedBox(
+                                              fit: BoxFit.fitHeight,
+                                              child: Text(
+                                                "3,334",
+                                                style: TextStyle(
+                                                    fontWeight: FontWeight.bold,
+                                                    color: Color(0xff2984e2)),
+                                              ),
+                                            ),
+                                          ])),
+                                ],
                               ),
                             ],
                           ),
-                        )),
-                  )
-                ],
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
               ),
-            ),
-          ]),
-        ));
+            ])));
   }
 }
