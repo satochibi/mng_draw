ff7f704364742154c612c0de25bebdfef06375d8
satochibi <satochibi0120@gmail.com>
2022-04-07 13:34:14 +0900

    editscreenのメニューバーをレスポンシブ化

--- lib/screens/edit_screen.dart
+++ lib/screens/edit_screen.dart
@@ -15,73 +15,77 @@ class EditScreen extends StatelessWidget {
         color: Colors.white,
         child: Column(
           children: [
-            Row(
-              children: [
-                TextButton(
-                  child: const Text("color"),
-                  onPressed: () {
-                    showDialog(
-                        context: context,
-                        builder: (context) {
-                          return const AlertDialog(
-                            title: Text("choose a color!"),
-                            content: ChooseColorScreen(),
-                          );
-                        });
-                  },
-                ),
-                TextButton(
-                    child: const Text("width"),
+            SizedBox(
+              width: double.infinity,
+              child: Wrap(
+                direction: Axis.horizontal,
+                children: [
+                  TextButton(
+                    child: const Text("color"),
                     onPressed: () {
                       showDialog(
                           context: context,
                           builder: (context) {
-                            return AlertDialog(
-                              title: const Text("line width"),
-                              content: const ChooseWidthScreen(),
-                              actions: [
-                                TextButton(
-                                    onPressed: () => Navigator.pop(context),
-                                    child: const Text("OK")),
-                              ],
+                            return const AlertDialog(
+                              title: Text("choose a color!"),
+                              content: ChooseColorScreen(),
                             );
                           });
-                    }),
-                TextButton(
-                    child: const Text("screentone"),
-                    onPressed: () {
-                      showDialog(
-                          context: context,
-                          builder: (context) {
-                            return AlertDialog(
-                              title: const Text("choose a screentone!"),
-                              content: const ChooseScreentoneScreen(),
-                              actions: [
-                                TextButton(
-                                    onPressed: () => Navigator.pop(context),
-                                    child: const Text("OK")),
-                              ],
-                            );
-                          });
-                    }),
-                TextButton(
-                    child: const Text("settings"),
-                    onPressed: () {
-                      showDialog(
-                          context: context,
-                          builder: (context) {
-                            return AlertDialog(
-                              title: const Text("screentone scale"),
-                              content: const ChooseSettingsScreen(),
-                              actions: [
-                                TextButton(
-                                    onPressed: () => Navigator.pop(context),
-                                    child: const Text("OK")),
-                              ],
-                            );
-                          });
-                    }),
-              ],
+                    },
+                  ),
+                  TextButton(
+                      child: const Text("width"),
+                      onPressed: () {
+                        showDialog(
+                            context: context,
+                            builder: (context) {
+                              return AlertDialog(
+                                title: const Text("line width"),
+                                content: const ChooseWidthScreen(),
+                                actions: [
+                                  TextButton(
+                                      onPressed: () => Navigator.pop(context),
+                                      child: const Text("OK")),
+                                ],
+                              );
+                            });
+                      }),
+                  TextButton(
+                      child: const Text("screentone"),
+                      onPressed: () {
+                        showDialog(
+                            context: context,
+                            builder: (context) {
+                              return AlertDialog(
+                                title: const Text("choose a screentone!"),
+                                content: const ChooseScreentoneScreen(),
+                                actions: [
+                                  TextButton(
+                                      onPressed: () => Navigator.pop(context),
+                                      child: const Text("OK")),
+                                ],
+                              );
+                            });
+                      }),
+                  TextButton(
+                      child: const Text("settings"),
+                      onPressed: () {
+                        showDialog(
+                            context: context,
+                            builder: (context) {
+                              return AlertDialog(
+                                title: const Text("screentone scale"),
+                                content: const ChooseSettingsScreen(),
+                                actions: [
+                                  TextButton(
+                                      onPressed: () => Navigator.pop(context),
+                                      child: const Text("OK")),
+                                ],
+                              );
+                            });
+                      }),
+                ],
+              ),
             ),
             Expanded(
               child: ArtBoard(
